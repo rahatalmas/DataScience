55b1110673364b86685c46ecbc0f8a740370fdec
rahatalmas <rahat.almas5599@gmail.com>
2023-11-26 05:54:52 +0600

    bigData

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request
+import pickle5 as pickle
+
+app = Flask(__name__)
+
+userInput = []
+
+@app.route("/", methods=["GET", "POST"])
+def hello():
+
+    userInput.clear()
+    if request.method == "POST":
+        variance = request.form.get("variance")
+        skewness = request.form.get("skewness")
+        curtosis = request.form.get("curtosis")
+        entropy = request.form.get("entropy")
+
+        userInput.append(variance)
+        userInput.append(skewness)
+        userInput.append(curtosis)
+        userInput.append(entropy)
+
+        # converting string to float values
+        for i in range(len(userInput)):
+            userInput[i] = float(userInput[i])
+        print("User input: ", userInput)
+
+        # testing our pickle file
+        with open('pickleOutput2', 'rb') as f:
+            mp = pickle.load(f)
+        print(mp)
+        pickle_test = mp.predict([userInput])
+        print("Predicted Output: ", pickle_test)
+
+        if pickle_test[0]==1:
+            print("pickle value",mp)
+            return render_template("trueBundle.html")
+        if pickle_test[0]==0:
+            print("pickle value",mp)
+            return render_template("falseBundle.html")
+
+    return render_template("index.html")
+
+
+'''
+@app.route("/verified/")
+def verified():
+    return render_template("trueBundle.html")
+
+@app.route("/not-verified/")
+def notVerified():
+    return render_template("falseBundle.html")
+'''
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
